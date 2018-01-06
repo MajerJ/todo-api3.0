2a93efade9bcc84d87d30d8d0d9ae859f209ae70
Jan Majer <33447429+MajerJ@users.noreply.github.com>
2018-01-06 23:39:17 +0100

    Delete loginRoutes.js

--- api/routes/loginRoutes.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-module.exports = function(app) {
-  var userList = require('../controllers/loginController');
-
-  app.route('/login')
-    .get(userList.create_a_user)
-    .post(userList.load_a_user);
-};
